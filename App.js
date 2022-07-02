39f4c9e757e4f8e211698cf62556def8933d105a
ardourApeX <joygupta501@gmail.com>
2022-07-02 15:12:49 +0530

    WIP : Adding home route

--- App.js
+++ App.js
@@ -1,14 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { NativeBaseProvider } from 'native-base';
-import { StyleSheet, Text, View } from 'react-native';
+import { extendTheme, NativeBaseProvider } from 'native-base';
+import { StyleSheet } from 'react-native';
+import Home from './pages/Home';
 
 export default function App() {
+	const theme = extendTheme({
+		backgroundColor: 'green.100',
+		colors: {
+			// Add new color
+			primary: {
+				green: '#097969',
+			},
+			// Redefinig only one shade, rest of the color will remain same.
+			amber: {
+				400: '#d97706',
+			},
+		},
+		config: {
+			// Changing initialColorMode to 'dark'
+			initialColorMode: 'dark',
+		},
+	});
+
 	return (
-		<NativeBaseProvider>
-			<View style={styles.container}>
-				<Text>Open up App.js to sstart working on your app!</Text>
-				<StatusBar style="auto" />
-			</View>
+		<NativeBaseProvider theme={theme}>
+			<Home style={{ border: '1px solid red' }} />
 		</NativeBaseProvider>
 	);
 }
--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,16 +1,35 @@
+import { Box, Button, Center, Container, Image, Text } from 'native-base';
 export default function Home() {
 	return (
 		<Center flex={1} px="3">
-			<Container>
-				<Heading>
-					A component library for the
-					<Text color="emerald.500"> React Ecosystem</Text>
-				</Heading>
-				<Text mt="3" fontWeight="medium">
-					NativeBase is a simple, modular and accessible component library that
-					gives you building blocks to build you React applications.
-				</Text>
-			</Container>
+			<Box
+				w={'80%'}
+				alignSelf={'center'}
+				borderColor={'primary.green'}
+				borderWidth="1px"
+				borderRadius="2%"
+				display="flex"
+				flexDirection="column"
+				justifyContent="space-evenly"
+			>
+				<Container>
+					<Image
+						alignSelf={'center'}
+						size={['xs', 'sm', 'md', 'lg', 'xl', 'xl']}
+						resizeMode="contain"
+						source={require('../assests/images/doctor.png')}
+					/>
+					<Text fontSize={['sm', 'sm', 'xl']} mt="3" fontWeight="medium">
+						NativeBase is a simple, modular and accessible component library
+						that gives you building blocks to build you React applications.
+					</Text>
+				</Container>
+				<Container>
+					<Button rounded success>
+						<Text>Success</Text>
+					</Button>
+				</Container>
+			</Box>
 		</Center>
 	);
 }
