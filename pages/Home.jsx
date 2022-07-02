37edf8a39ec27519c4aea386e2b18e80d500a554
ardourApeX <joygupta501@gmail.com>
2022-07-02 13:16:31 +0530

    Working Intitial Codebase

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,1 +1,16 @@
-function Home() {}
+export default function Home() {
+	return (
+		<Center flex={1} px="3">
+			<Container>
+				<Heading>
+					A component library for the
+					<Text color="emerald.500"> React Ecosystem</Text>
+				</Heading>
+				<Text mt="3" fontWeight="medium">
+					NativeBase is a simple, modular and accessible component library that
+					gives you building blocks to build you React applications.
+				</Text>
+			</Container>
+		</Center>
+	);
+}
